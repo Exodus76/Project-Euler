dcb029c3c487c523d2b9a2ee214020a978624ae1
Mridul <mhayaran123@gmail.com>
2019-01-02 14:52:44 +0530

    remove print statements

--- Project_Euler/2/two.java
+++ Project_Euler/2/two.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class two{
 	public static void main(String[] args) {
 	long i,cout,t1 = 0, t2 = 1,even=0;
-	Scanner sc = new Scanner(System.in);
-	int var = sc.nextInt();
-	for(i=0;i<=var;i++){
+	long var = 4000000;
+	for(i=0;i<var;i++){
 		cout = t1 + t2; //1 2
 		t1 = t2;
 		t2 = cout;
-		if(cout%2 == 0){
+		if (cout>var){
+			break;
+		}
+		else if(cout%2 == 0){
 			even = even + cout;
-			System.out.println(even);
-			continue;
 		}
 	}
 	System.out.println(even);
